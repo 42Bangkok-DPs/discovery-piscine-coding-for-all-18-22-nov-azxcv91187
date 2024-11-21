1183db9b42d34e24a0d5cf25f8798acf929753eb
azxcv91187 <vlovemoandmy@gmail.com>
2024-11-21 13:33:50 +0000

    Finish!

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,5 @@
+print("Enter a number")
+y = int(input())
+for x in range(0,y + 2):
+    z = x * y
+    print(x,"x",y,"=",z)
---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,7 @@
+import sys
+if len(sys.argv) > 1:
+    argument = sys.argv[1]
+    print("none")
+else:
+    for i in range (0,11):
+        print("Table de", i,":", 0, i+(0*i), i+(1*i),i+(2*i),i+(3*i),i+(4*i),i+(5*i),i+(6*i),i+(7*i),i+(8*i),i+(9*i))
